cc0ff864a983a2eb379bf74cf3dd864bb4c87d68
abuj43221-wq <abuj43221@gmail.com>
2026-04-22 14:14:47 +0530

    Update chek_channel_sub.py

--- src/app/filters/chek_channel_sub.py
+++ src/app/filters/chek_channel_sub.py
@@ -1,14 +1,22 @@
 import asyncpg
 from aiogram import Bot
-from aiogram.filters import Filter, BaseFilter
+from aiogram.filters import BaseFilter
 from aiogram.types import Message, CallbackQuery
-from asyncpg import Connection
 
 from src.app.database.queries.channels import ChannelDataBaseActions
 
 
 class CheckSubscription(BaseFilter):
-    async def __call__(self, event: Message | CallbackQuery, pool: asyncpg.Pool, bot: Bot, **kwargs):
+
+    async def __call__(self, event: Message | CallbackQuery, **data):
+
+        pool: asyncpg.Pool = data.get("pool")
+        bot: Bot = data.get("bot")
+
+        # 🔥 SAFE CHECK
+        if pool is None or bot is None:
+            return False
+
         channel_actions = ChannelDataBaseActions(pool)
         channel_data = await channel_actions.get_all_channels()
 
@@ -18,7 +26,12 @@ class CheckSubscription(BaseFilter):
         for channel in channel_data:
 
             if channel[3] == "True":
-                user_status = await bot.get_chat_member(channel[0], event.from_user.id)
+                user_status = await bot.get_chat_member(
+                    channel[0],
+                    event.from_user.id
+                )
+
                 if user_status.status not in ["member", "administrator", "creator"]:
                     return True
+
         return False
