d71018950f47f0c09d7d6f7c2db2fddf32789a09
abuj43221-wq <abuj43221@gmail.com>
2026-04-22 12:29:55 +0530

    Update db_url.py

--- src/app/common/db_url.py
+++ src/app/common/db_url.py
@@ -1,8 +1,11 @@
+import os
 from src.app.core.config import Settings
-
-
 def construct_postgresql_url(settings: Settings):
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url:
+        return database_url
     postgresql_dsn = (
-        f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}"f"/{settings.db_name}"
+f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}"
+        f"/{settings.db_name}"
     )
     return postgresql_dsn
