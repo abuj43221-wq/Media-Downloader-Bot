c241b2489aa4e7e44dcc0b5121d1cf144502ce4e
abuj43221-wq <abuj43221@gmail.com>
2026-04-22 14:27:48 +0530

    Update db_url.py

--- src/app/common/db_url.py
+++ src/app/common/db_url.py
@@ -1,7 +1,7 @@
 import os
 from src.app.core.config import Settings
 def construct_postgresql_url(settings: Settings):
-    database_url = os.environ.get("DATABASE_URL")
+    database_url = os.environ.get("DATABASE_URL", "postgresql://media_1rb3_user:2oUMuOnjKhiDpDOid0SW5TfaBpiQTowQ@host:5432/media_1rb3")
     if database_url:
         return database_url
     postgresql_dsn = (
